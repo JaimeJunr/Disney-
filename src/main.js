2949dfda39a64d3fbcf95b3039ccc5162e21a917
jaime <jaimeodairbassojuniorjaime@gmail.com>
2024-03-13 15:51:57 -0300

    added faq section and its features

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
 // Inicia a func no Load do DOM
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
     
     for (let i = 0; i < buttons.length; i++) {
         // Adiciona a func Click nos tab-Bottons
         buttons[i].addEventListener('click', function(btn){
             const tabTarget = btn.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${tabTarget}]`);
-            TabsDisplay();
+            tabsRemoveDisplay();
             aba.classList.add('show__list--is-active');
             removeBtnActive();
             btn.target.classList.add('show__tabs__button--is-active');
+        })
+    }
+
+    for (let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', function(qu){
+            const classe = 'faq__questions__item--is-open'
+            const parentElem = qu.target.parentNode;
+
+            parentElem.classList.toggle(classe);
         })
     }
 })
@@ -23,7 +33,7 @@ function removeBtnActive() {
     }
 }
 
-function TabsDisplay() {
+function tabsRemoveDisplay() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
     
     for (let i = 0; i < tabsContainer.length; i++) {

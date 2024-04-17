063950bddf8a6f74f9d482dd4deecb5e07aa0f6f
jaime <jaimeodairbassojuniorjaime@gmail.com>
2024-04-17 18:16:19 -0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,33 +1,64 @@
 // Inicia a func no Load do DOM
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const questions = document.querySelectorAll('[data-faq-question]')
-    
-    for (let i = 0; i < buttons.length; i++) {
-        // Adiciona a func Click nos tab-Bottons
-        buttons[i].addEventListener('click', function(btn){
-            const tabTarget = btn.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${tabTarget}]`);
-            tabsRemoveDisplay();
-            aba.classList.add('show__list--is-active');
-            removeBtnActive();
-            btn.target.classList.add('show__tabs__button--is-active');
-        })
-    }
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const heightHero = heroSection.clientHeight;
+
+    //Header
+    window.addEventListener('scroll', function(){
+
+        
+        const position = window.scrollY;
+        if (position > heightHero){
+            hiddenHeader();
+            console.log(heightHero)
+        } else{
+            displayHeader();
+        }
+    })
+
+    //FAQ
     for (let i = 0; i < questions.length; i++){
         questions[i].addEventListener('click', function(qu){
             const classe = 'faq__questions__item--is-open'
-            const parentElem = qu.target.parentNode;
+                const parentElem = qu.target.parentNode;
+                
+                parentElem.classList.toggle(classe);
+            })
+        }
+    
 
-            parentElem.classList.toggle(classe);
-        })
-    }
-})
+    //Showns
+    for (let i = 0; i < buttons.length; i++) {
+            // Adiciona a func Click nos tab-Bottons
+            buttons[i].addEventListener('click', function(btn){
+                const tabTarget = btn.target.dataset.tabButton;
+                const aba = document.querySelector(`[data-tab-id=${tabTarget}]`);
+                tabsRemoveDisplay();
+                aba.classList.add('show__list--is-active');
+                removeBtnActive();
+                btn.target.classList.add('show__tabs__button--is-active');
+            })
+        }
+    })
+
+    
+
+function  hiddenHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function  displayHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function removeBtnActive() {
     const buttons = document.querySelectorAll('[data-tab-button');
-
+    
     for (let i=0; i < buttons.length; i++) {
         buttons[i].classList.remove('show__tabs__button--is-active')
     }
